7794db84a5c6e4603afa8f74a01d774aa7f47d9b
sanjay kumar <100122199+sanjay3131@users.noreply.github.com>
2025-04-22 10:29:34 +0530

    Update generateToken.js

--- server/genetareToken/generateToken.js
+++ server/genetareToken/generateToken.js
@@ -5,11 +5,15 @@ export const generateToken = (id, res) => {
     const token = jwt.sign({ id }, process.env.JWT_SECRET, {
       expiresIn: "30d",
     });
-    res.cookie("token", token, {
+
+    const cookieOptions = {
       httpOnly: true,
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-       sameSite: "none",
-      secure: true, // Ensure secure cookies in production});
+      sameSite: "none",
+      secure: true, // Ensure secure cookies in production
+    };
+
+    res.cookie("token", token, cookieOptions);
   } catch (error) {
     console.error(`Token generation error: ${error.message}`);
     res.status(500).json({
