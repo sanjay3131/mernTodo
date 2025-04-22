a410403bde55d0fed7dd08957dbfdb524d2fde6a
sanjay3131 <sanjay31ramuk@gmail.com>
2025-04-22 15:06:29 +0530

    cors orgin changed

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // your local frontend
+    origin: "https://mern-todo-eta-henna.vercel.app/", // your local frontend
     credentials: true, // allow cookies
   })
 );
