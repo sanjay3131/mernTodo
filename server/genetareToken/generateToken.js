7782a75ff255e27ee46ba1c179eaf74a42a17036
sanjay kumar <100122199+sanjay3131@users.noreply.github.com>
2025-04-22 09:35:39 +0530

    Update generateToken.js

--- server/genetareToken/generateToken.js
+++ server/genetareToken/generateToken.js
@@ -9,7 +9,7 @@ export const generateToken = (id, res) => {
       httpOnly: true,
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
       secure: process.env.NODE_ENV === "production", // Use secure cookies in production
-      sameSite: "strict", // Prevent CSRF attacks
+      sameSite: "none", // Prevent CSRF attacks
     });
   } catch (error) {
     console.error(`Token generation error: ${error.message}`);
