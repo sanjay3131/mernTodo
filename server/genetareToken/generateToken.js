e2e36b71db997d1c82a027d5183a9aa700f0fad0
sanjay kumar <100122199+sanjay3131@users.noreply.github.com>
2025-04-22 10:26:15 +0530

    Update generateToken.js

--- server/genetareToken/generateToken.js
+++ server/genetareToken/generateToken.js
@@ -8,9 +8,8 @@ export const generateToken = (id, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
-      sameSite: process.env.NODE_ENV === "production" ? "None" : "Strict", // "None" for cross-origin cookies in production
-    });
+       sameSite: "none",
+      secure: true, // Ensure secure cookies in production});
   } catch (error) {
     console.error(`Token generation error: ${error.message}`);
     res.status(500).json({
